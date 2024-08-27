ce2572445e138330b50c8acc79c869eec391f458
Brandon <112407387+bholmdev@users.noreply.github.com>
2024-08-26 23:40:20 -0500

    final build

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,26 @@ function createAccountAlert() {
 }
 
 $(function() {
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("toggle");
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("toggle");
+    })
+})
+
+$(function() {
+    $("#createAccountButton").click(function() {
+        $("#createAccountModal").modal("toggle");
+    })
+})
+
+$(function() {
     $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    $("#carouselButton").click(function(){
+    $("#carouselButton").click(function() {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
